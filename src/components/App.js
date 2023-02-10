f0da4eac4f9992abaf69c0bb5d9f56e19bc86b48
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2023-02-10 16:57:37 -0600

    edit app

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import TeaControl from "./TeaControl";
+
+function App(){
+  return ( 
+    <div className="container">
+      <Header />
+      <TeaControl />
+    </div>
+  );
+}
+
+
+export default App;
